c3bc8bf7afb4cef05a5cfa12b0e51e5272f7e107
FLighter7 <21360067+FLighter7@users.noreply.github.com>
2023-08-24 21:54:54 +0200

    feat: add csr example

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-export const load = () => null;
+import express, {Express} from 'express';
+import {middleware404, middlewareErrorHandler} from 'common/be/express-middlewares';
+import {Fs} from 'common/be/services';
+
+/**
+ * CSR example (backend part)
+ */
+export const load = async (name: string): Promise<Express> => {
+	const app = express();
+	const distPath = `${Fs.rootPath}/apps/${name}/${name}-frontend/dist`;
+
+	app.use(express.static(distPath));
+
+	app.get(['/', '/:page'], async (req, res, next) => {
+		const file = `${distPath}/index.html`;
+
+		return await Fs.exists(file)
+			? res.sendFile(file)
+			: next();
+	});
+
+	app.use(middleware404(name));
+	app.use(middlewareErrorHandler(name));
+
+	return app;
+};
